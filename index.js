64228817b5ee829cc6ff34eb70823f6bc6dd82db
Branden <brandenehman45@gmail.com>
2021-07-02 11:41:47 -0600

    Got the random ninja gen to work

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const URL = "http://localhost:3000/Ninjas"
+const URL = "http://localhost:3000/Ninjas";
+const firstTeam = [];
+const secondTeam = [];
+const clientsTeam = document.querySelector('#clients-team');
+const enemiesTeam = document.querySelector('#enemy-team');
+const li = document.createElement('li');
 
-fetch(URL)
-.then(resp => resp.json())
-.then(data => {
-    const clientTeam = document.querySelector('#clients-team');
-    const enemyTeam = document.querySelector("#enemy-team");
-    const teamMember = `$`
-    console.log(enemyTeam)
-})
+function grabbingNinjas() {
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(data => {
+    teamPicker(data)
+    });
+};
+grabbingNinjas()
+function randomNumberGenerator(max) {
+    return Math.floor(Math.random() * max);
+};
+
+function teamPicker(ninjas) {
+    while (firstTeam.length <= 2) {
+        const randomNinja = ninjas[randomNumberGenerator(ninjas.length)];
+        firstTeam.push(randomNinja.Name)
+    };
+    while (secondTeam.length <= 2) {
+        const randomEnemy = ninjas[randomNumberGenerator(ninjas.length)];
+        secondTeam.push(randomEnemy.Name)
+    };
+};
